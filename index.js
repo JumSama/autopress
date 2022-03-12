3b981d093b599d2ceb931f8c64964301dc51eab4
JumSama <1274001055@qq.com>
2022-03-12 17:34:25 +0800

    v1.0.0

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const HelpConfig = require('./lib/core/help')
 const VersionConfig = require('./lib/core/version')
 const createCommands = require('./lib/core/create')
 
+// 帮助
 HelpConfig()
+// 版本查看
 VersionConfig()
+// 命令
 createCommands()
 
 program.parse(process.argv)
